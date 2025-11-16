01c134e95b5b39a3258a82a0b0358503e9aa97ef
hammadshabbir10 <hammadshabbir507@gmail.com>
2025-11-16 00:33:57 -0500

    Activate enf

---
+++ activate_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "🐍 Activating Python Virtual Environment..."
+source securechat-env/bin/activate
+echo "✅ Virtual environment activated!"
+echo "💡 Run 'deactivate' to exit the virtual environment"
